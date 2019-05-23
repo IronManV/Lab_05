53b297189d709a4ebf1e46193d4fdb3e68ab7e02
pawel.gawin <pawel.gawin@PK4-8>
2019-05-23 09:38:22 +0200

    Added new Lab_05

---
+++ Cw_01/Cw_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat kitty = new Cat();
+            kitty.IncreaseAge();
+            Console.WriteLine(kitty.Age);
+            kitty.CountLegs();
+
+            Console.ReadKey();
+        }
+    }
+
+    abstract class Animal
+    {
+        public uint Age { get; set; } = 0;
+
+        public void IncreaseAge()
+        {
+            Age++;
+        }
+    }
+
+    interface ILandBased
+    {
+        void CountLegs();
+    }
+
+    class Cat : Animal, ILandBased
+    {
+        public string Sound { get; set; } = "miau";
+        public void CountLegs()
+        {
+            Console.WriteLine("Cat has four legs and falls on all of them!");
+        }
+    }
+}
---
+++ Cw_05/Cw_05/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cw_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employed employee1 = new Employed();
+            employee1.Age = 20;
+            employee1.Salary = 4000.0;
+
+            Boss employee2 = new Boss();
+            employee2.Age = 40;
+            employee2.Salary = 8000.0;
+            employee2.BonusSalary = 1000.0;
+
+            Console.WriteLine("Salaries are set!");
+
+            Console.ReadKey();
+        }
+    }
+    abstract class Human
+    {
+        public uint Age { get; set; }
+    }
+
+    class Employed : Human
+    {
+        public double Salary { get; set; }
+    }
+
+    class Boss : Employed
+    {
+        public double BonusSalary { get; set; }
+    }
+}
